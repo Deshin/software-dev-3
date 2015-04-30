65d1c26ad7448efbceb899b58e334ff3099ea6ad
Sarah Ward <456263@students.wits.ac.za>
2015-04-30 20:37:36 +0200

    Added some comments

--- scripts/SqliteWrapper.py
+++ scripts/SqliteWrapper.py
@@ -4,11 +4,13 @@ from WrapperBase import WrapperBase
 import sqlite3
 
 class SqliteWrapper(WrapperBase):
+#Initialisation
     def __init__(self, database):
         self._database=database
         self._cur=[]
         self._con=[]
-        
+
+#Function allowing a connection to a database to be established        
     def connect(self):
         try:
             self._con=sqlite3.connect(self._database)
@@ -18,25 +20,26 @@ class SqliteWrapper(WrapperBase):
             print "Error %s:" %e.args[0]
             raise
         
+#Function allowing one to disconnect from a database       
     def disconnect(self):
         self._con.close()
         print "Disconnecting from the Database"
-        
+
+#Function to perform an SQL query and return any results if applicable        
     def query(self, queryString):
         try:
             self._cur.execute(queryString)
             if queryString.startswith("SELECT"):
                 items=self._cur.fetchall()  
-                return items
-                      
+                return items                      
         except sqlite3.Error,e:
             print "Error %s:" %e.args[0]
             raise
         print "The query string is "+queryString
         
     
-if __name__=='__main__':
-    wrapper=SqliteWrapper("arb database")
-    wrapper.connect()
-    wrapper.query('SELECT SQLITE_VERSION()')
-    wrapper.disconnect()
+# if __name__=='__main__':
+#     wrapper=SqliteWrapper("arb database")
+#     wrapper.connect()
+#     wrapper.query('SELECT SQLITE_VERSION()')
+#     wrapper.disconnect()
