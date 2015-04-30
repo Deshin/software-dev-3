480f3edac187822d841bf12889c613008e4683ca
Deshin Subramoney <536916@students.wits.ac.za>
2015-04-30 19:33:58 +0200

    Implementing partials for frontend

--- www/js/ko/index.js
+++ www/js/ko/index.js
@@ -1,9 +1,32 @@
 function RootViewModel() {
 	var self = this;
 	self.user = ko.observable("User");
+
+	self.getVM = function(path) {
+		return function(callback) {
+			console.log('getting ' + '/js/ko/viewmodels/'+path+'.json');
+			$.getJSON('/js/ko/viewmodels/'+path+'.json', function(data) {
+				console.log('got it!');
+				console.log(data);
+				callback(data.getViewModel());
+			})
+			.done(function() {
+				console.log( "second success" );
+			})
+			.fail(function( jqxhr, textStatus, error ) {
+				var err = textStatus + ", " + error;
+				console.log( "Request Failed: " + err );
+			})
+			.always(function() {
+			console.log( "complete" );
+			});
+		}
+	}
 }
 
 rootViewModel = new RootViewModel();
+
+pager.Href.hash = '#!/';
 
 pager.extendWithPage(rootViewModel);
 
