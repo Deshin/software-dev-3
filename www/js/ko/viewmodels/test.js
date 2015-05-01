d1736d3fae4e4bdebce835566c3f4618ae9f0c41
Deshin Subramoney <536916@students.wits.ac.za>
2015-05-01 12:52:09 +0200

    Changed the test

--- www/js/ko/viewmodels/test.js
+++ www/js/ko/viewmodels/test.js
@@ -1,5 +1,12 @@
-define(["knockout"], function(ko) {
-	var vm = {};
+define(["jquery", "knockout"], function($, ko) {
+	var vm = this;
 	vm.message = ko.observable("Test Message");
+	vm.getDocs = function() {
+		console.log("clicked!");
+		vm.message("Loading!");
+		$.get('/docs/rest_api', function(data) {
+			vm.message(data);
+		});
+	}
 	return vm;
 });
