420c8c9b45c064223f1a989b60fe40e91828d2fe
Anthony Farquharson <563648@students.wits.ac.za>
2015-05-01 16:04:31 +0200

    Updated page to load id and display object

--- www/js/ko/viewmodels/publicationDetails.js
+++ www/js/ko/viewmodels/publicationDetails.js
@@ -1,17 +1,20 @@
 define(["jquery", "knockout"], function($, ko) {
 	var vm = this;
-	vm.message = ko.observable("Test Message");
-	vm.publicationID = ko.observable(null);
+	vm.publication = ko.observable({});
 	vm.getDocs = getDocs;
+
+	getDocs(1);
 
 	return vm;
 
-	function getDocs() {
-		console.log("clicked!");
-		vm.message("Loading!");
-		$.getJSON('/api/publicationDetails.py?id=' + vm.publicationID(), function(data) {
-			vm.message(data);
+	function getDocs(ID) {
+		vm.publication("Loading!");
+
+		$.getJSON('/api/publicationDetails.py?id=' + ID, function(data) {
+			vm.publication(data);
+			console.log(data);
 		});
+
 	}
 
 });
