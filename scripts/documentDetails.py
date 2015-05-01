02781d3e6fb857685e0ff6c993109890d4db9ba5
Sarah Ward <456263@students.wits.ac.za>
2015-05-01 14:00:22 +0200

    fixed a bug

--- scripts/documentDetails.py
+++ scripts/documentDetails.py
@@ -1,10 +1,10 @@
 from RestApi import RestApi
 from SqliteWrapper import SqliteWrapper
 
-def main():
+def main(id):
     db = SqliteWrapper()
     rest = RestApi(db)
-    result = rest.getDocumentDetails("1")
+    result = rest.getDocumentDetails(id)
 
 if __name__ == "__main__":
-    main()
+    main("1")
