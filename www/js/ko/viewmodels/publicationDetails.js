cbdfdaea0b06d450caf15bf9034b4a6605f68ae5
Anthony Farquharson <563648@students.wits.ac.za>
2015-05-01 23:33:18 +0200

    Made publication details section more robust.

--- www/js/ko/viewmodels/publicationDetails.js
+++ www/js/ko/viewmodels/publicationDetails.js
@@ -1,11 +1,16 @@
 define(["jquery", "knockout"], function($, ko) {
 	var vm = this;
-	vm.publication = ko.observable({});
-	vm.pubId = ko.observable({});
+	vm.publication = ko.observable(null);
+	vm.pubId = ko.observable();
+	vm.statusMsg = ko.observable('');
 
 	vm.pubId.subscribe(function(newVal) {
+		vm.publication(null);
+		vm.statusMsg = 'Loading Publication.';
 		$.getJSON('/api/publicationDetails.py?id=' + newVal, function(data) {
 			vm.publication(data);
+		}).fail(function() {
+			vm.statusMsg = "Error, Publication not found.";
 		});
 	});
 
