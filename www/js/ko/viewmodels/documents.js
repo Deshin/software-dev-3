7cac54996cd524955c72835c262bd559a020410f
Anthony Farquharson <563648@students.wits.ac.za>
2015-05-02 00:33:43 +0200

    Beautifying the front end.

--- www/js/ko/viewmodels/documents.js
+++ www/js/ko/viewmodels/documents.js
@@ -5,7 +5,10 @@ define(["jquery", "knockout"], function($, ko) {
 		for (var i = 0; i < data.length; i++) {
 			var authors = "";
 			for (var j = 0; j < data[i].Authors.length; j++) {
-				authors += data[i].Authors[j].Initials + " " + data[i].Authors[j].Surname + ", ";
+				authors += data[i].Authors[j].Initials + " " + data[i].Authors[j].Surname;
+				if(j != data[i].Authors.length - 1) {
+					authors += ", ";
+				}
 			}
 			data[i].Authors = authors;
 			data[i].link = "#!/publicationDetails?pubId="+data[i].PublicationId.toString();
--- www/js/ko/viewmodels/publicationDetails.js
+++ www/js/ko/viewmodels/publicationDetails.js
@@ -8,14 +8,29 @@ define(["jquery", "knockout"], function($, ko) {
 		vm.publication(null);
 		vm.statusMsg('Loading Publication.');
 		$.getJSON('/api/publicationDetails.py?id=' + newVal, function(data) {
+			// Fetch the publication information
+		}).done(function(data) {
+			var authors = "";
+			for (var j = 0; j < data.Authors.length; j++) {
+				authors += data.Authors[j][4] + " " + data.Authors[j][2];
+				if(j != data.Authors.length - 1) {
+					authors += ", ";
+				}
+			}
+			data.Authors = authors;
 			vm.publication(data);
-		}).done(function() {
 			vm.statusMsg("Success!");
-		}).fail(function() {
+		}).fail(function(err) {
 			vm.statusMsg("Error, Publication not found.");
 		});
 	});
 
 	return vm;
+
+	function parseAuthor(authors) {
+		for(var i = 0; i < Authors.length; i++) {
+
+		}
+	}
 
 });
