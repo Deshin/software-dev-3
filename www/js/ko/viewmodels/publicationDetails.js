f8e198bb1e0c0b6fd6f175867ecf742caac97a14
Anthony Farquharson <563648@students.wits.ac.za>
2015-05-01 14:27:47 +0200

    Added window to enter ID for a publication

--- www/js/ko/viewmodels/publicationDetails.js
+++ www/js/ko/viewmodels/publicationDetails.js
@@ -1,10 +1,11 @@
 define(["jquery", "knockout"], function($, ko) {
 	var vm = this;
 	vm.message = ko.observable("Test Message");
+	vm.publicationID = ko.observable(0);
 	vm.getDocs = function() {
 		console.log("clicked!");
 		vm.message("Loading!");
-		$.getJSON('/api/publicationDetails.py', function(data) {
+		$.getJSON('/api/publicationDetails.py?id=' + vm.publicationID(), function(data) {
 			vm.message(data);
 		});
 	};
