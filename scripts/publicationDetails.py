447d535e4e4a4b11f9d391f744132751620f0245
Anthony Farquharson <563648@students.wits.ac.za>
2015-05-01 14:14:40 +0200

    Added required header information to python script

--- scripts/publicationDetails.py
+++ scripts/publicationDetails.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from RestApi import RestApi
 from SqliteWrapper import SqliteWrapper
 
@@ -5,6 +7,9 @@ def main(id):
     db = SqliteWrapper()
     rest = RestApi(db)
     result = rest.getDocumentDetails(id)
+    print "Content-Type: application/json"
+    print ""
+    print result    
 
 if __name__ == "__main__":
     main("1")
