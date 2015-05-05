f2fda5fb8a2ff636defae6e4a995d22eb5aef14a
Sarah Ward <456263@students.wits.ac.za>
2015-05-05 17:51:15 +0200

    Laying out function structure for the ru1 use case

--- scripts/insertions.py
+++ scripts/insertions.py
@@ -13,11 +13,25 @@ def main(details):
         print "Content-Type: text/html"
         print ""
         print "The document could not be added to the database"
-    print "Content-Type: application/json"
+    print "Content-Type: text/html"
     print "Success"
     print result    
 
 if __name__ == "__main__":
     form=cgi.FieldStorage()
-    id=form.getlist('details')
+    details=form.getlist('details')
+    details={"Title": "This document is testing conference paper insertion",
+             "Category": "Conference paper",
+             "Year": 2015,
+             "Publisher": "Testing publishers", 
+             "TableOfContentsPath": "conferences\PRASA2014\TOC\PRASA2014",
+             "ScanPath": "conferences\PRASA2014\Publications\TestingInsertion",
+             "Accreditation": "Not Accredited",
+             "Abstract": "It is important to have a document to test adding to the database",
+             "MotivationForAccreditation": "Please accredit me!",
+             "PeerReview": "People read it :)",
+             "PathToFile": "",
+             "DocumentTitle": "",
+             "ConferenceTitle": "PRASA2014",
+             "Country": "South Africa"}
     main(details)
--- scripts/ru1.py
+++ scripts/ru1.py
@@ -3,4 +3,25 @@
 import json
 
 def insertDocument(self, details):
-    print "Hello"
+    try:
+        if details["Category"].lower().startswith("conference"):
+            insertConferencePaper(self,details)
+            
+        elif details["Category"].lower().startswith("journal"):
+            insertJournalPaper(self,details)
+            
+        elif details["Category"].lower().startswith("book"):
+            insertBookSection(self,details)
+        
+    except:
+        return "404"
+        
+def insertConferencePaper(self,details):
+    print details
+    
+def insertJournalPaper(self,details):
+    print details
+    
+def insertBookSection(self,details):
+    print details
+    
