e5e7703df04e5d81b562db8d74c4dbeb2ba4bec2
Anthony Farquharson <563648@students.wits.ac.za>
2015-05-09 23:35:48 +0200

    Fixed form layout a bit, need to complete the layout for different types of publication.

--- www/js/ko/viewmodels/publicationUpload.js
+++ www/js/ko/viewmodels/publicationUpload.js
@@ -1,24 +1,46 @@
 define(["jquery", "knockout"], function($, ko) {
 	var vm = this;
 
+  vm.submitPublication = submitPublication;
   var publication = {};
+  vm.categoryList = ko.observableArray(['Journal Article', 'Conference Paper', 'Other']);
 
-  vm.Title = ko.observable(publication.Title);
-  vm.Abstract = ko.observable(publication.Abstract);
-  vm.Authors = ko.observable(publication.Authors);
-  vm.Category = ko.observable(publication.Category);
-  vm.ConferenceTitle = ko.observable(publication.ConferenceTitle);
-  vm.Country = ko.observable(publication.Country);
-  vm.DocumentTitle = ko.observable(publication.DocumentTitle);
-  vm.MotivationForAccreditation = ko.observable(publication.MotivationForAccreditation);
-  vm.PeerReviewProcess = ko.observable(publication.PeerReviewProcess);
-  vm.Publisher = ko.observable(publication.Publisher);
-  vm.Year = ko.observable(publication.Year);
+  init();
 
 	return vm;
 
   function submitPublication() {
+    console.log("Submit the form");
+    vmToJson();
+    console.log(publication);
+  }
 
+  function init() {
+    vm.Title = ko.observable("");
+    vm.Abstract = ko.observable("");
+    vm.Authors = ko.observable("");
+    vm.Category = ko.observable("");
+    vm.ConferenceTitle = ko.observable("");
+    vm.Country = ko.observable("");
+    vm.DocumentTitle = ko.observable("");
+    vm.MotivationForAccreditation = ko.observable("");
+    vm.PeerReviewProcess = ko.observable("");
+    vm.Publisher = ko.observable("");
+    vm.Year = ko.observable("");
+  }
+
+  function vmToJson() {
+    publication.Title = vm.Title();
+    publication.Abstract = vm.Abstract();
+    publication.Authors = vm.Authors();
+    publication.Category = vm.Category();
+    publication.ConferenceTitle = vm.ConferenceTitle();
+    publication.Country = vm.Country();
+    publication.DocumentTitle = vm.DocumentTitle();
+    publication.MotivationForAccreditation = vm.MotivationForAccreditation();
+    publication.PeerReviewProcess = vm.PeerReviewProcess();
+    publication.Publisher = vm.Publisher();
+    publication.Year = vm.Year();
   }
 
 });
