0f25f5e2c69c62770c136c6c088890c608d6de36
Sarah Ward <456263@students.wits.ac.za>
2015-05-12 16:56:16 +0200

    Implimented insertion of books and journals

--- scripts/insertions.py
+++ scripts/insertions.py
@@ -7,31 +7,31 @@ import cgi
 def main(details):
     db = SqliteWrapper()
     rest = RestApi(db)
-    result = rest.insertDocument(details)
-    if result=="404":
+    result= rest.insertDocument(details)
+    if result[0]=="404":
         print "Status:404"
         print "Content-Type: text/html"
         print ""
         print "The document could not be added to the database"
+        print result[1]
     else:
         print "Status:200"
         print "Content-Type: text/html"
         print "Success"
-        print result    
 
 if __name__ == "__main__":
     form=cgi.FieldStorage()
     details=form.getlist('publication')
-    details={"Title": "This document is testing conference paper insertion",
-             "Category": "Conference paper",
+    details={"Title": "This document is testing book section insertion",
+             "Category": "Book section",
              "Year": 2015,
+             "Chapter":3,
              "Publisher": "Testing publishers", 
-             "TableOfContentsPath": "conferences\PRASA2014\TOC\PRASA2014-3",
-             "ScanPath": "conferences\PRASA2014\Publications\TestingInsertion3",
+             "TableOfContentsPath": "books\arbBook\TOC\TestingInsertion",
+             "ScanPath": "books\arbBook\Publications\TestingInsertion3",
              "Accreditation": "Not Accredited",
              "Abstract": "It is important to have a document to test adding to the database",
-             "MotivationForAccreditation": "Please accredit me!",
-             "PeerReview": "People read it :)",
-             "ConferenceTitle": "PRASA",
-             "Country": "South Africa"}
+             "BookTitle": "PRASA",
+             "ISBN":1234567,
+             "Type":"Accredited"}
     main(details)
--- scripts/ru1.py
+++ scripts/ru1.py
@@ -5,17 +5,17 @@ import sys
 
 def insertDocument(self, details):
     if details["Category"].lower().startswith("conference"):
-        try:
-            print details["PathToFile"]
-        except:
+        if "PathToFile" not in details:
             details["PathToFile"]=None
-        try:
-            print details["DocumentTitle"]
-        except:
+        if "DocumentTitle" not in details:
             details["DocumentTitle"]=None
         result=insertConferencePaper(self,details)
         
     elif details["Category"].lower().startswith("journal"):
+        if "Volume" not in details:
+            details["Volume"]=None
+        if "Issue" not in details:
+            details["Issue"]=None
         result=insertJournalPaper(self,details)
         
     elif details["Category"].lower().startswith("book"):
@@ -46,24 +46,79 @@ def insertExistingConference(self,details, conferenceID):
             self._databaseWrapper.query("INSERT INTO ConferencePublicationPeerReviewDocumentation(ConferencePublicationDetailID,PathToFile,DocumentTitle) VALUES(?,?,?)",(conferencePublicationDetailID,details["PathToFile"],details["DocumentTitle"]))
         #this commit must be at the end to make the process atomic
         self._databaseWrapper.commit()
+        return 200
     except:
-        print sys.exc_info()[1]
-        return "404"
+        return "404",sys.exc_info()[1]
 
 def insertNewConference(self,details):
     try:
        self._databaseWrapper.query("INSERT INTO Conferences(ConferenceTitle, Year, Country) VALUES(?,?,?)",(details["ConferenceTitle"],details["Year"], details["Country"]))
        conferenceID=self.databaseWrapper._cur.lastrowid
        insertExistingConference(self,details,conferenceID)
+       return "200"
     except:
-        print sys.exc_info()[1]
-        return "404"
+        return "404",sys.exc_info()[1]
  
     
 def insertJournalPaper(self,details):
-    print details
+    existingJournal=self._databaseWrapper.query("SELECT * FROM Journals WHERE JournalTitle=?",[details["JournalTitle"]])
+    if existingJournal!=[]:
+        journalID=existingJournal[0][0]
+        result=insertExistingJournal(self,details,journalID)
+    else:
+        result=insertNewJournal(self,details)
+    return result
+
+def insertExistingJournal(self,details,journalID):
+    try:
+        #note: although this is repeated code from conference insertion, it is important
+        #that it is repeated here to ensure atomicity of insertions
+        self._databaseWrapper.query("INSERT INTO Publications(Title,Category,Year,Publisher,TableOfContentsPath,ScanPath,Accreditation) VALUES(?,?,?,?,?,?,?)",(details["Title"],details["Category"],details["Year"],details["Publisher"], details["TableOfContentsPath"], details["ScanPath"], details["Accreditation"]))
+        publicationID=self.databaseWrapper._cur.lastrowid
+        self._databaseWrapper.query("INSERT INTO JournalPublicationDetail(JournalID,PublicationID,Volume,Issue,Abstract) VALUES(?,?,?,?,?)",(journalID, publicationID, details["Volume"], details["Issue"], details["Abstract"]))
+        #this commit must be at the end to make the process atomic
+        self._databaseWrapper.commit()
+        return "200"
+    except:
+        return "404", sys.exc_info()[1]
+
+def insertNewJournal(self,details):
+    try:
+       self._databaseWrapper.query("INSERT INTO Journals(JournalTitle, ISSN, HIndex,Type) VALUES(?,?,?,?)",(details["JournalTitle"],details["ISSN"], details["HIndex"], details["Type"]))
+       journalID=self.databaseWrapper._cur.lastrowid
+       insertExistingJournal(self,details,journalID)
+       return "200"
+    except:
+        return "404",sys.exc_info()[1]
     
 def insertBookSection(self,details):
-    print details
+    existingBook=self._databaseWrapper.query("SELECT * FROM Books WHERE BookTitle=?",[details["BookTitle"]])
+    if existingBook!=[]:
+        bookID=existingBook[0][0]
+        result=insertExistingBook(self,details,bookID)
+    else:
+        result=insertNewBook(self,details)
+    return result
     
     
+def insertExistingBook(self,details,bookID):
+    try:
+        #note: although this is repeated code from conference insertion, it is important
+        #that it is repeated here to ensure atomicity of insertions
+        self._databaseWrapper.query("INSERT INTO Publications(Title,Category,Year,Publisher,TableOfContentsPath,ScanPath,Accreditation) VALUES(?,?,?,?,?,?,?)",(details["Title"],details["Category"],details["Year"],details["Publisher"], details["TableOfContentsPath"], details["ScanPath"], details["Accreditation"]))
+        publicationID=self.databaseWrapper._cur.lastrowid
+        self._databaseWrapper.query("INSERT INTO BookPublications(PublicationID,Chapter,Abstract, BooksID) VALUES(?,?,?,?)",(publicationID, details["Chapter"], details["Abstract"], bookID))
+        #this commit must be at the end to make the process atomic
+        self._databaseWrapper.commit()
+        return "200"
+    except:
+        return "404", sys.exc_info()[1]
+
+def insertNewBook(self,details):
+    try:
+       self._databaseWrapper.query("INSERT INTO Books(BookTitle, ISBN,Type) VALUES(?,?,?)",(details["BookTitle"],details["ISBN"], details["Type"]))
+       bookID=self.databaseWrapper._cur.lastrowid
+       insertExistingBook(self,details,bookID)
+       return "200"
+    except:
+        return "404",sys.exc_info()[1]    
