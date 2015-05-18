cfb0791608b39b9a9509583772f7e4b49fed1d2e
Sarah Ward <456263@students.wits.ac.za>
2015-05-19 00:01:02 +0200

    bug fix?

--- scripts/insertions.py
+++ scripts/insertions.py
@@ -16,7 +16,6 @@ def main(details):
         print "Content-Type: text/html"
         print ""
         print "The document could not be added to the database"
-        print result[1]
     else:
         print "Status:200"
         print "Content-Type: text/html"
