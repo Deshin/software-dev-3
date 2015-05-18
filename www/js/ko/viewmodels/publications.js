afd70008b008d6c4abeb210627e7528a1d2e7a16
Deshin Subramoney <536916@students.wits.ac.za>
2015-05-19 00:54:06 +0200

    Removed logging in publications

--- www/js/ko/viewmodels/publications.js
+++ www/js/ko/viewmodels/publications.js
@@ -1,7 +1,6 @@
 define(["jquery", "knockout"], function($, ko) {
 	var vm = this;
 	vm.gotData = function(data) {
-		console.log(data);
 		vm.publications.removeAll();
 		for (var i = 0; i < data.length; i++) {
 			var authors = "";
@@ -19,7 +18,6 @@ define(["jquery", "knockout"], function($, ko) {
 	vm.search = ko.observable(null);
 	vm.search.subscribe(function(newVal) {
 		if (newVal != "") {
-			console.log("Searching for: ", newVal);
 			rootViewModel.search(newVal);
 			$.getJSON('/api/publications.py?simpleSearch='+vm.search(), vm.gotData);
 		} else {
