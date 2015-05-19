90935248ecc3862920d26709c3bf095eeef97eeb
Jacques Visser <jcqvisser@gmail.com>
2015-05-19 14:00:37 +0200

    G5: Completed simpleSearch and fixed pubs endpoint
    (•_•)
    ( •_•)>⌐■-■
    (⌐■_■)

--- scripts/restSearch.py
+++ scripts/restSearch.py
@@ -2,16 +2,18 @@ import json
 
 def simpleSearch(self, searchTerm):
     # searchTerms is a list of strings
-    query = "SELECT  * "\
-        "FROM Publications "\
+    query = "SELECT  Publications.* "\
+        "FROM Publications JOIN Authors "\
         "WHERE "\
-        "Publications.Title LIKE ?"
-    pubs = self._databaseWrapper.query(query, ('%'+searchTerm+'%',))
+        "Authors.FirstName LIKE ? OR "\
+        "Publications.Title Like ? "\
+        "GROUP BY Publications.Title "
+    pubs = self._databaseWrapper.query(query, ('%'+searchTerm+'%','%'+searchTerm+'%'))
     if pubs == []:
         return "200"
     data = []
     for i in range(0,len(pubs)):
-        auths = self._databaseWrapper.query("SELECT * FROM Authors WHERE (FirstName LIKE ? OR Surname LIKE ?) AND PublicationID="+str(pubs[i][0]), [('%'+searchTerm+'%') for k in range(0,2)])
+        auths = self._databaseWrapper.query("SELECT * FROM Authors WHERE PublicationID=? ", (str(pubs[i][0]),))
         auth = []
         for j in range(0, len(auths)):
             auth.append({"ID":auths[j][0], "PublicationID":auths[j][1], "First Name" : auths[j][2], "Surname" : auths[j][3],  "Initials" : auths[j][4]})
