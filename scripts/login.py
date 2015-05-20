699bbcbf89f307f1072f921acbf99ffdad9a3c36
Sarah Ward <456263@students.wits.ac.za>
2015-05-20 12:11:19 +0200

    removed uneccesary line

--- scripts/login.py
+++ scripts/login.py
@@ -30,5 +30,4 @@ if __name__ == "__main__":
     form = cgi.FieldStorage()
     username = form.getvalue("username", None)
     hash = form.getvalue("hash", None)
-    password="f53f5a42cad02d80e8db99e87a2f176d09f6dfdf8da5ff5a21a78e74e16734df"
     main(username,password)  
