6cd546b3983ada4c1f0e0b256d8057e529fecaf6
Jacques Visser <jcqvisser@gmail.com>
2015-05-20 17:35:52 +0200

    G7: bugs

--- scripts/restSearch.py
+++ scripts/restSearch.py
@@ -78,8 +78,14 @@ def advancedSearch(self, searchTerms, skip, length, sortBy, sort):
     queryValues.append(skip)
 
     pubs = self._databaseWrapper.query(query, queryValues)
-    result = addAuthors(self, pubs)
-    return json.dumps(result)
+    data = []
+    for i in range(0,len(pubs)):
+        auths = self._databaseWrapper.query("SELECT * FROM Authors WHERE PublicationID=? ", (str(pubs[i][0]),))
+        auth = []
+        for j in range(0, len(auths)):
+            auth.append({"ID":auths[j][0], "PublicationID":auths[j][1], "First Name" : auths[j][2], "Surname" : auths[j][3],  "Initials" : auths[j][4]})
+        data.append({"PublicationId" : pubs[i][0], "Title" : pubs[i][1], "Category" : pubs[i][2], "Year" : pubs[i][3], "Publisher" :pubs[i][4], "Authors" : auth})
+    return json.dumps(data)
 
 def splitAdvancedSearchTerms(terms):
     oldEntries = terms
