9ccd5ff6d9560581d65707dd036f9f668c06a796
Deshin Subramoney <536916@students.wits.ac.za>
2015-05-20 14:26:21 +0200

    G7: Fixed the binding on the observables

--- www/js/ko/viewmodels/advancedSearch.js
+++ www/js/ko/viewmodels/advancedSearch.js
@@ -2,12 +2,15 @@ define(["jquery", "knockout"], function($, ko) {
 	var vm = this;
 	//title, author, bookTitle, conferenceTitle, journalTitle, abstract
 	vm.fields = ko.observableArray(['title', 'author', 'bookTitle', 'conferenceTitle', 'journalTitle', 'abstract']);
+	$.getJSON("/api/advancedSearchFields.py", function(data) {
+		vm.fields(data);
+	});
 	vm.operators = ko.observableArray(['equals', 'contains']);
 	vm.defaultSearch = function() {
 		return 	{
-			field: "title",
-			value: "",
-			operator: "equals"
+			field: ko.observable("title"),
+			value: ko.observable(""),
+			operator: ko.observable("equals")
 		};
 	};
 	vm.searches = ko.observableArray([vm.defaultSearch()]);
@@ -21,8 +24,13 @@ define(["jquery", "knockout"], function($, ko) {
 	vm.searchHref = ko.computed(function() {
 		var srchs = [];
 		for (var i = 0; i < vm.searches().length; i++) {
-			if (!(vm.searches()[i].value)) {
-				srchs.push(vm.searches()[i]);
+			if (vm.searches()[i].value) {
+				var newVal = {
+					field: vm.searches()[i].field(),
+					value: vm.searches()[i].value(),
+					operator: vm.searches()[i].operator()
+				}
+				srchs.push(newVal);
 			};
 		};
 		return '/#!/publications?search='+JSON.stringify(srchs);
